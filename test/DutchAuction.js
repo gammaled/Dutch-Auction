6d5b50545582c8c13a8ce7295b71b677ea4963f5
gammaled <guleduni@yahoo.co.uk>
2021-12-30 11:15:41 +0000

    declared seller, bidder and contractInstance variable. Instantiate new contract before every test.

--- test/DutchAuction.js
+++ test/DutchAuction.js
@@ -1,1 +1,8 @@
 const DutchAuction = artifacts.require("DutchAuction");
+contract("DutchAuction", (accounts) => {
+    let [seller, bidder] = accounts;
+    let contractInstance;
+    beforeEach(async () => {
+        contractInstance = await DutchAuction.new();
+    })
+})
