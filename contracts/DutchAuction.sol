b0bdc686ed0b05db397a6ef3e30166fc6c9182e4
gammaled <guleduni@yahoo.co.uk>
2021-12-30 13:34:41 +0000

    created boilerplate of DutchAuction contract using the Ownable contract as auction

--- contracts/DutchAuction.sol
+++ contracts/DutchAuction.sol
@@ -2,3 +2,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/b0cf6fbb7a70f31527f36579ad644e1cf12fdf4e/contracts/access/Ownable.sol";
+
+contract DutchAuction is Ownable {
+
+}
