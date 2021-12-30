7626a41352f6e1308073db63949ea5e9ea385945
gammaled <guleduni@yahoo.co.uk>
2021-12-30 14:16:51 +0000

    changed import statement to import from node-packages instead of github

--- contracts/DutchAuction.sol
+++ contracts/DutchAuction.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/b0cf6fbb7a70f31527f36579ad644e1cf12fdf4e/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DutchAuction is Ownable {
 
