3c38cb2374497ec6e3fa54e7d686274b0a066286
gammaled <guleduni@yahoo.co.uk>
2021-12-30 11:48:44 +0000

    added tests to check value of variables after contract initialisation

--- test/DutchAuction.js
+++ test/DutchAuction.js
@@ -4,5 +4,16 @@ contract("DutchAuction", (accounts) => {
     let contractInstance;
     beforeEach(async () => {
         contractInstance = await DutchAuction.new();
+    });
+    context("when initialised", async () => {
+        it("should have the startPrice variable not be equal to or lower than reservePrice variable", async () => {
+            // TODO: Test if startPrice > reservePrice
+        })
+        it("should have the endTime variable greater than startTime variable", async () => {
+            // TODO: Test endTime > startTime
+        })
+        it("should have the owner as the address that initialised it", async () => {
+            // TODO: Test if msg.sender == the address that initialised contract
+        })
     })
 })
