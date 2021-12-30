6f955b5f61269fdbe372f931690259adf6928514
gammaled <guleduni@yahoo.co.uk>
2021-12-30 13:33:13 +0000

    imported Ownable contract from OpenZeppelin repo

--- contracts/DutchAuction.sol
+++ contracts/DutchAuction.sol
@@ -1,2 +1,4 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/b0cf6fbb7a70f31527f36579ad644e1cf12fdf4e/contracts/access/Ownable.sol";
