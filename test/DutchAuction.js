4466e8a11296dcfd256a99d4e2908bcde843733e
gammaled <guleduni@yahoo.co.uk>
2022-01-01 17:10:43 +0000

    Changed statement to compare result of owner() to seller() getter funciton

--- test/DutchAuction.js
+++ test/DutchAuction.js
@@ -25,7 +25,7 @@ contract("DutchAuction", (accounts) => {
         it("should have the owner as the address that initialised it", async () => {
             // TODO: Check why it passes without being implemented in smart contract
             let result = await contractInstance.owner();
-            expect(result).to.equal(seller);
+            expect(result).to.equal(contractInstance.seller());
         })
         xit("should define startTime variable with the value of the timestamp of contract deployment", async () => {
             // TODO: Fix logic to check if (NOW <= blockTime <= NOW + 15 sec)
