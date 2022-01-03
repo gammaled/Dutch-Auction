4e9f1e32f7943a4b988a9c910bf3d7572410039e
gammaled <guleduni@yahoo.co.uk>
2022-01-03 08:32:54 +0000

    Added console log in test

--- test/DutchAuction.js
+++ test/DutchAuction.js
@@ -57,6 +57,8 @@ contract("DutchAuction", (accounts) => {
         })
         xit("should only allow a caller (bidder) that has the startPrice in their wallet", async () => {
             //TODO: Test to check balance of msg.sender (bidder) has the required capital to enter bid
+            console.log(await contractInstance.OwnershipTransferred());
+            console.log(await contractInstance.OwnershipTransferred()._maxListeners == !undefined);
         })
         xit("should have a currentPrice is equal to or less than startPrice", async () => {
             //TODO: Test that ownership transfer to msg.sender(bidder) works
