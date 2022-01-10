c2e33f1b0610ac6e04e6b41c7f46d7b28ffcaa41
gammaled <guleduni@yahoo.co.uk>
2022-01-10 14:45:53 +0000

    Renamed transfer function

--- contracts/TestERCTokens.sol
+++ contracts/TestERCTokens.sol
@@ -3,12 +3,16 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TestERCToken is ERC721 {
+
+    string private _name;
+    string private _symbol;
+
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
         _symbol = symbol_;
     }
 
-    function _safeMint(address to, uint256 tokenId) internal virtual {
+    function _transferAsset(address to, uint256 tokenId) internal virtual {
         _safeMint(to, tokenId, "");
     }
 
