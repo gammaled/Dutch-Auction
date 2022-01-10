33e971f10c2f8ac5a8ac9877d0e897ed6f251789
gammaled <guleduni@yahoo.co.uk>
2022-01-10 14:36:16 +0000

    Created contract as ERC721 and included constructor function

--- contracts/TestERCTokens.sol
+++ contracts/TestERCTokens.sol
@@ -1,3 +1,10 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TestERCToken is ERC721 {
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    }
+}
