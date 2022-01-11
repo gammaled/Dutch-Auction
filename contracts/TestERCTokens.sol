36563c26af81197f7ac32474d23080a17d75f826
gammaled <guleduni@yahoo.co.uk>
2022-01-11 10:42:59 +0000

    Changed IERC721 to ERC721 import

--- contracts/TestERCTokens.sol
+++ contracts/TestERCTokens.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract TestERCToken is IERC721 {
+contract TestERCToken is ERC721 {
 
     string private _name;
     string private _symbol;
