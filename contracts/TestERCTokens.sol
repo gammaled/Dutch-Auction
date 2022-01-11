3a499fc480e3173944ddc22253bb40fbc985823b
gammaled <guleduni@yahoo.co.uk>
2022-01-11 10:53:04 +0000

    Declared the storage variable called tokenCounter

--- contracts/TestERCTokens.sol
+++ contracts/TestERCTokens.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TestERCToken is ERC721 {
-
+    uint256 public tokenCounter;
     constructor() public ERC721 ("Test","TST") {
 
     }
