5d529c0413ca67876509cc5ed51edde04ec48543
gammaled <guleduni@yahoo.co.uk>
2022-01-11 10:53:51 +0000

    Initialised tokenCounter with the value 0 in constructor

--- contracts/TestERCTokens.sol
+++ contracts/TestERCTokens.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract TestERCToken is ERC721 {
     uint256 public tokenCounter;
     constructor() public ERC721 ("Test","TST") {
-
+        tokenCounter = 0;
     }
 
     function safeTransferFrom(
